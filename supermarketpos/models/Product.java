45355f0cef984a2069cec090418b62852137cb72
Anthony Abeo <anthonyabeo@gmail.com>
2016-10-31 22:08:41 +0000

    Product class implemented

--- supermarketpos/models/Product.java
+++ supermarketpos/models/Product.java
@@ -11,4 +11,69 @@ package supermarketpos.models;
  */
 public class Product {
     
+    private String productName;
+    private double price;
+    private int quantity;
+    
+    public Product(String productName, double price, int quantity){
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    
+    /**
+     * A method that returns the name of a product.
+     * 
+     * @return productName the name of the product as a String.
+     */
+    public String getProductName(){
+        return productName;
+    }
+    
+    /**
+     * A method that returns the price of a product.
+     * 
+     * @return price the name of the product as a double.
+     */
+    public double getPrice(){
+        return price;
+    }
+    
+    /**
+     * A method that returns the quantity of a product.
+     * 
+     * @return quantity the name of the product as a int.
+     */
+    public int getQuantity(){
+        return quantity;
+    }
+    
+  
+    /**
+     * A method updates the name of a product to a new value.
+     * 
+     * @param newProductName the new value of the product as a String.
+     */
+    public void setProductName(String newProductName){
+        productName = newProductName;
+    }
+    
+    /**
+     * A method updates the price of a product to a new value.
+     * 
+     * @param newPrice the new value of the product as a double.
+     */
+    public void setPrice(double newPrice){
+        price = newPrice;
+    }
+    
+    /**
+     * A method updates the quantity of a product to a new value.
+     * 
+     * @param newQuantity the new value of the product as a int.
+     */
+    public void setQuantity(int newQuantity){
+        quantity = newQuantity;
+    }
+    
 }
