b67bc142329bc88ca9aee3ba50f1b2d8a412ee24
Alieu Jallow <Alieu Jallow>
2016-11-01 23:35:28 +0000

    editproduct controller set

--- supermarketpos/controllers/EditProductController.java
+++ supermarketpos/controllers/EditProductController.java
@@ -5,10 +5,40 @@
  */
 package supermarketpos.controllers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
  * @author Anthony
  */
-public class EditProductController {
+public class EditProductController implements ActionListener
+{
+    EditProductView editProductView;
+    int productID;
+    public EditProductController(EditProductView epv,int id)
+    {
+        editProductView=epv;
+        productID=id;
+        editProductView.getCancelButton().addActionListener(this);
+        editProductView.getSaveButton().addActionListener(this);
+        editProductView.getCancelButton().setActionCommand("cancel");
+        editProductView.getsaveButton().setActionCommand("save");      
+    }
     
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getActionCommand().equalsIgnoreCase("cancel"))
+        {
+            editProductView.setVisible(false);
+        }
+        else if(e.getActionCommand().equalsIgnoreCase("save"))
+        {
+            String productName= editProductView.getNameTextField().getText();
+            int quantity = Integer.parseInt(editProductView.getQuantityTextField().getText());
+            double price = Double.parseDouble(editProductView.getPriceTextField().getText());
+            Database.getInstance().updateProduct(productName,price,quantity,productID);
+            editProductView.setVisible(false);
+        }
+    }
 }
