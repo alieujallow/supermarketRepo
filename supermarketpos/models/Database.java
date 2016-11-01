fd67c609ebfe892dab651cf9bc515699bf8402c6
Anthony Abeo <anthonyabeo@gmail.com>
2016-11-01 23:06:03 +0000

    added methods to update products and validate employee login in credentials

--- supermarketpos/models/Database.java
+++ supermarketpos/models/Database.java
@@ -5,10 +5,53 @@
  */
 package supermarketpos.models;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author Anthony
  */
 public class Database {
+    
+    public void updateProduct(String productName, double price, int quantity, int productid){
+        try{ 
+            PreparedStatement ps = conn.prepareStatement("UPDATE records SET productName=?, price=?,"
+                    + "quantity=? WHERE productID="+"'"+productid+"'");
+            
+            ps.setString(2, productName);
+            ps.setDouble(3, price);
+            ps.setInt(4, quantity);
+            
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    } 
+    
+    
+    public boolean validateEmployee(String username, String password){
+        boolean isValid = false;
+        try{
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM "
+                              + "employees WHERE userName="+"'"+username+"'");
+            
+            if(resultSet != null){
+                if(resultSet.getString(3).equalsIgnoreCase("clerk") && 
+                            resultSet.getString(5).equalsIgnoreCase(password)){
+                    isValid = true;
+                } 
+            }
+        }
+        catch(Exception ex){
+            System.err.println(ex);
+        }
+        
+        return isValid;
+    }
+    
+    
     
 }
